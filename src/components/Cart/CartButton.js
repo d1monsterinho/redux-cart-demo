016c973e107c47327af94b2bac6b1b8acccf97dc
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-07-10 17:18:56 +0300

    Wire UI slice to CartButton.js; Create cart state slice

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
 import classes from './CartButton.module.css';
+import {useDispatch} from "react-redux";
+import {uiSliceActions} from "../../store/ui-slice";
 
 const CartButton = (props) => {
+    const dispatch = useDispatch();
+
+    const handleCartButtonClick = () => {
+        dispatch(uiSliceActions.toggleCartVisibility());
+    }
+
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={handleCartButtonClick}>
             <span>My Cart</span>
             <span className={classes.badge}>1</span>
         </button>
---
+++ src/store/cart-slice.js
@@ -0,0 +1,48 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const addItemActionCreator = (state, action) => {
+    const newItem = action.payload;
+    const existingItem = state.items.find(item => item.id === newItem.id);
+
+    if (!existingItem) {
+        state.items.push({
+            ...newItem,
+            quantity: 1,
+            total: newItem.price,
+        });
+    } else {
+        existingItem.quantity++;
+        existingItem.total += newItem.price;
+    }
+}
+
+const removeItemActionCreator = (state, action) => {
+    const items = state.items;
+    const existingItemIndex = items.findIndex(item => item.id === action.payload);
+
+    if (items[existingItemIndex].quantity === 1) {
+        items.splice(existingItemIndex, 1);
+    } else {
+        const existingItem = items[existingItemIndex];
+        existingItem.quantity--;
+        existingItem.total -= existingItem.price;
+    }
+}
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+        totalSize: 0,
+    },
+    reducers: {
+        addItem(action, state) {
+            addItemActionCreator(action, state);
+        },
+        removeItem(action, state) {
+            removeItemActionCreator(action, state);
+        },
+    },
+});
+
+export const cartSliceActions = cartSlice.actions;
