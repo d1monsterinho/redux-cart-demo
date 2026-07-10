5c17250acc32487155df28f3cea317f531518c8c
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-07-10 17:30:57 +0300

    Reformat Products.js and ProductItem.js; Add dummy products and pass them to ProductItem.js

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,38 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+    {
+        id: 'p1',
+        title: 'My First Book',
+        price: 10,
+        description: 'This is My First Book Description!',
+    },
+    {
+        id: 'p2',
+        title: 'My Second Book',
+        price: 15,
+        description: 'This is My Second Book Description!',
+    },
+    {
+        id: 'p3',
+        title: 'My Third Book',
+        price: 20,
+        description: 'This is My Third Book Description!',
+    },
+];
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(item => (
+                    <ProductItem key={item.id} {...item} />
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
